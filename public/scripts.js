0fe42e0397b502fa9d192841b20c7841406067f3
Antonio Decanini <antonio.decanini@hotmail.com>
2020-03-19 21:22:32 -0300

    Add server

---
+++ public/scripts.js
@@ -0,0 +1,23 @@
+const cards = document.querySelectorAll('.cards');
+const showHideButton = document.querySelectorAll('.show-hide');
+
+for (let card of cards) {
+  card.addEventListener("click", function() {
+    const id = card.getAttribute('id');
+    window.location.href = `/receita/${id}`
+  });
+}
+
+for (let button of showHideButton) {
+  button.addEventListener("click", function() {
+    idTopic = button.getAttribute('id');
+    
+    if (document.querySelector(`.${idTopic}`).getAttribute('class') == `${idTopic}`) {
+      document.querySelector(`.${idTopic}`).classList.add('deactive');
+      button.innerHTML = 'Mostrar';
+    } else {
+      document.querySelector(`.${idTopic}`).classList.remove('deactive');
+      button.innerHTML = 'Ocultar';
+    }
+  });
+}
